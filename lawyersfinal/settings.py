836e86849a59068f335d77bfb3ec597e43fe30b4
Asma Abufayah <aaha44@gmail.com>
2022-06-15 12:16:11 +0300

    p

--- lawyersfinal/settings.py
+++ lawyersfinal/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG',True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lawyers-con.herokuapp.com']
+ALLOWED_HOSTS = ['lawyers-con.herokuapp.com','127.0.0.1']
 
 # Application definition
 
