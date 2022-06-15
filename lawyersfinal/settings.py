f30e28dd3ad90470845d1f2fff361d899ac7e97e
Asma Abufayah <aaha44@gmail.com>
2022-06-15 12:31:31 +0300

    p

--- lawyersfinal/settings.py
+++ lawyersfinal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-kup^k0vtjw%*w!nb(ya9j*sro6$xi_tct7&(-*+t@qxk@(9lld')
-DEBUG = os.environ.get('DJANGO_DEBUG',True)
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lawyers-con.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['lawyers-con.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -123,8 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
